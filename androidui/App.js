3f262a468f604ecb6fdb909262e4bb50a8ae7b3b
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2022-08-18 15:31:25 +0530

    "Elearning Ui"

--- androidui/App.js
+++ androidui/App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./src/screens/Home";
+import Courses from "./src/screens/Courses";
+import Xd from "./src/screens/Xd";
+import VideoPage from "./src/screens/VideoPage";
+
+const Stack = createStackNavigator();
+const screenOptionStyle = {
+  headerShown: false
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={screenOptionStyle}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Courses" component={Courses} />
+        <Stack.Screen name="Xd" component={Xd} />
+        <Stack.Screen name="VideoPage" component={VideoPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
